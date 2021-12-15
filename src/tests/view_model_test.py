40340d1b38ce360f8bddb979e408cb249565bd9e
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2021-12-15 22:45:37 +0200

    Update tests, adding requested empty init file to entities

--- src/tests/view_model_test.py
+++ src/tests/view_model_test.py
@@ -1,11 +1,24 @@
 import unittest
-from view.login_view import LoginView
 from viewmodel.view_model import ViewModel
+from model.user_model import UserModel
+from database import db
 
 
 class TestUserModel(unittest.TestCase):
     def setUp(self) -> None:
         self.view_model = ViewModel()
+        self.user_model = UserModel()
+        self.database = db
 
     def test_viewmodel_init(self) -> None:
         self.assertEqual(self.view_model._user_logged_in, None)
+
+    def test_viewmodel_create_user(self) -> None:
+        username = 'viewmodeltestuser'
+        password = 'password'
+        self.view_model.create_user(username, password, password)
+        created_users = self.user_model._get_all_users()
+        self.assertEqual(username in created_users, True)
+
+    def tearDown(self) -> None:
+        self.database.empty_database()
